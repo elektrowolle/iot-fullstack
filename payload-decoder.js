79bae6c1a2c3ed85d9501eb35b2870dece3d36a4
Benjamin Skirlo <elektrowolle@users.noreply.github.com>
2017-01-23 12:06:54 +0100

    Update payload-decoder.js

--- payload-decoder.js
+++ payload-decoder.js
@@ -2,8 +2,8 @@ function Decoder(bytes, port) {
   // Decode an uplink message from a buffer
   // (array) of bytes to an object of fields.
   var decoded = {corner:[]};
-
-  // if (port === 1) decoded.led = bytes[0];
+  
+  //Reorder bytes received from node: LSB to MSB. Note that we receive uint16_t.
   decoded.corner[0] = (bytes[1] << 8) + bytes[0];
   decoded.corner[1] = (bytes[3] << 8) + bytes[2];
   decoded.corner[2] = (bytes[5] << 8) + bytes[4];
